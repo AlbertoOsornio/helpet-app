49c23947f8ccc573cfb1b177f551ec4ecafc8f07
Rafa Osornio <osornio.rafael@gmail.com>
2019-08-16 16:47:52 -0500

    Updates

--- precache-manifest.466874b7a620ccdfb5f92e8d1d538848.js
+++ precache-manifest.466874b7a620ccdfb5f92e8d1d538848.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "25e419a1f900ce2051c6ff91fda5fd45",
+    "revision": "ea2de9ebfa6ff2e120e9fa816c16c54f",
     "url": "/helpet-app/index.html"
   },
   {
-    "revision": "4b227d3e184c1bccb0e1",
-    "url": "/helpet-app/static/css/main.3aebd33a.chunk.css"
+    "revision": "510bd726eb95e6261cfc",
+    "url": "/helpet-app/static/css/main.ba2acc9a.chunk.css"
   },
   {
     "revision": "f2cbe6783e3642e623af",
     "url": "/helpet-app/static/js/2.df432480.chunk.js"
   },
   {
-    "revision": "4b227d3e184c1bccb0e1",
-    "url": "/helpet-app/static/js/main.ea23c78a.chunk.js"
+    "revision": "510bd726eb95e6261cfc",
+    "url": "/helpet-app/static/js/main.7eb7e1cf.chunk.js"
   },
   {
     "revision": "83d5625be82db6ff1ba4",
